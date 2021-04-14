9c9d2f3388a80dd2a02bca030dd3b454f8c335f2
Ankitvish78 <46099614+Ankitvish78@users.noreply.github.com>
2021-04-14 13:29:34 +0530

    Music Company Website

---
+++ about.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<!--Code by Divinector (www.divinectorweb.com)-->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>How to Create responsive Homepage</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="about.css">    
+</head>
+<body>
+    <header>
+        <h2><a href="#">MUSIC</a></h2>
+        <nav>
+            <li><a href="INDEX1.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="index.html">Music</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </nav>
+    </header>
+
+    
+
+     <section class="about-area" id="about">
+        <h3 class="section-title">About Us</h3>
+        <ul class="about-content">
+            <li class="about-left"></li>
+            <li class="about-right">
+                <h2>About Our Company</h2>
+                <p>The Indian Music Industry (IMI) is a trust that represents the recording industry distributors in India. It was founded on February 28, 1936 as Indian Phonographic Industry (IPI). It is the 2nd oldest music industry organization in the world that was involved in protecting copyrights of music producers and supporting growth of music entertainment industry.</p>
+                <p>Led at felis arcu. Integer lorem lorem, tincidunt eu congue et, mattis ut ante. Nami suscipit, lectus id efficitur ornare, leo libero convalis nulla, vitae dignissim .
+                 University Faculties organise teaching and research into individual subjects.
+                 The University is rich in history - its famous buildings attract visitors.
+                 50 years of people, and achievements that continue to transform.
+                 The University's core values are as follows:freedom of thought.</p>
+                <a href="" class="about-btn">Learn more</a>
+            </li>
+        </ul>
+    </section>
+
+    <div class="container">
+    <h1 class="heading"><span>meet</span>Our Team</h1>
+
+    <div class="profiles">
+      <div class="profile">
+        <img src="ankur.jpeg" class="profile-img">
+
+        <h3 class="user-name">Ankur</h3>
+        <h5>Creative Director</h5>
+        <p>A creative director is a position often found within the graphic design, film, music, video game, fashion, advertising, media, or entertainment industries, but may be useful in other creative organizations such as web development and software development firms as well.</p>
+      </div>
+      <div class="profile">
+        <img src="arjun.jpg" class="profile-img">
+
+        <h3 class="user-name">Arjun</h3>
+        <h5>Managing Partner</h5>
+        <p>The managing partner is an agent of the partnership, having the authority to act on behalf of the company in buying and selling, signing contracts, and dealing with employees.</p>
+      </div>
+      <div class="profile">
+        <img src="rahul.jpg" class="profile-img">
+
+        <h3 class="user-name">Rahul</h3>
+        <h5>Project Manager</h5>
+        <p>In the broadest sense, project managers (PMs) are responsible for planning, organizing, and directing the completion of specific projects for an organization while ensuring these projects are on time, on budget, and within scope.</p>
+      </div>
+    </div>
+  </div>
+
+    <section class="contact-area" id="contact">
+        <h3 class="section-title">Contact</h3>
+        <ul class="contact-content">
+            <li>
+                <i class="fa fa-map-marker"></i>
+                <p>Mumbai, Maharashtra <br>India</p>
+            </li>
+
+            <li>
+                <i class="fa fa-phone"></i>
+                <p>1234567890 <br>1234567890</p>
+            </li>
+
+            <li>
+                <i class="fa fa-envelope"></i>
+                <p>ancd@gmail.com<br>abcd.com</p>
+            </li>
+        </ul>
+    </section>
+
+    <footer>
+        <p>All Right Reserved</p>
+    </footer>
+</body>
+</html>
---
+++ contact.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<!--Code by Divinector (www.divinectorweb.com)-->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>How to Create responsive Homepage</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="contact.css">    
+</head>
+<body>
+    <header>
+        <h2><a href="INDEX1.php">MUSIC</a></h2>
+        <nav>
+            <li><a href="INDEX1.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="index.html">Music</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </nav>
+    </header>
+
+    <section class="contact-area" id="contact">
+        <h3 class="section-title">Contact Us</h3>
+        <ul class="contact-content">
+            <li>
+                <i class="fa fa-map-marker"></i>
+                <p>Mumbai, Maharashtra <br>India</p>
+            </li>
+
+            <li>
+                <i class="fa fa-phone"></i>
+                <p>+91-1234567890 <br>+91-555.555.5555</p>
+            </li>
+
+            <li>
+                <i class="fa fa-envelope"></i>
+                <p>ancd@gmail.com<br>abcd.com</p>
+            </li>
+        </ul>
+    </section>
+
+    <footer>
+        <p>All Right Reserved</p>
+    </footer>
+</body>
+</html>
